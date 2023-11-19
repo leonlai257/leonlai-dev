749d7bed632b6a128c38a60ee77c1cc803a04cfe
leonlai257 <leonlai257@gmail.com>
2023-11-19 00:13:40 -0500

    feat(fix): fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,124 @@
 import {
-    OrbitControls,
-    PerspectiveCamera,
-    Plane,
-    Sky,
-    Sphere,
-} from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
-import { createRef, useState } from 'react';
-import * as THREE from 'three';
-export interface MainProps {}
+  Box,
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  Text,
+  useCursor,
+} from "@react-three/drei";
+import { ThreeElements, useFrame } from "@react-three/fiber";
+import type { NextPage } from "next";
+import { ReactNode, Suspense, useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
-const Main = (props: MainProps) => {
-    const { camera } = useThree();
-    const cameraRef = createRef();
+const Word = ({
+  children,
+  originalColor,
+  transitionColor,
+  ...props
+}: {
+  children: ReactNode;
+  originalColor: THREE.ColorRepresentation;
+  transitionColor?: THREE.ColorRepresentation;
+  outlineWidth?: number;
+  outlineColor?: string;
+}) => {
+  const color = new THREE.Color();
+  const ref = useRef<any>();
+  const [hovered, setHovered] = useState(false);
+  const over = (e: any) => (e.stopPropagation(), setHovered(true));
+  const out = () => setHovered(false);
 
-    const [fov] = useState(75);
-
-    return (
-        <>
-            <Sky />
-
-            <Sphere
-                position={[0, 1, 0]}
-                args={[1, 32, 32]}
-                castShadow
-                receiveShadow>
-                <meshStandardMaterial attach="material" color="white" />
-            </Sphere>
-            <Plane
-                rotation={[-Math.PI / 2, 0, 0]}
-                args={[10, 10]}
-                receiveShadow>
-                <meshStandardMaterial
-                    attach="material"
-                    color="white"
-                    side={THREE.DoubleSide}
-                />
-            </Plane>
-
-            <PerspectiveCamera
-                makeDefault
-                ref={cameraRef}
-                fov={fov}
-                near={0.1}
-                far={1000}
-                position={[0, 0, 10]}
-            />
-
-            <OrbitControls enableZoom={true} enableRotate={true} />
-        </>
+  // Change the mouse cursor on hover
+  useEffect(() => {
+    if (hovered) {
+      document.body.style.cursor = "pointer";
+    } else {
+      document.body.style.cursor = "auto";
+    }
+  }, [hovered]);
+  // Tie component to the render-loop
+  useFrame(({ camera }) => {
+    // Make text face the camera
+    ref.current.quaternion.copy(camera.quaternion);
+    // Animate font color
+    ref.current.material.color.lerp(
+      color.set(
+        hovered
+          ? transitionColor
+            ? transitionColor
+            : originalColor
+          : originalColor
+      ),
+      0.1
     );
+  });
+
+  return (
+    <Text
+      ref={ref}
+      onPointerOver={over}
+      onPointerOut={out}
+      onClick={() => console.log("clicked")}
+      {...props}
+    >
+      {children}
+    </Text>
+  );
 };
 
-export default Main;
+const Contact = () => {
+  const primaryColor = "#8294C4";
+  const secondaryColor = "#ACB1D6";
+  const tertiaryColor = "#DBDFEA";
+  const quaternaryColor = "#FFEAD2";
+
+  const [hovered, setHoverStatus] = useState(false);
+  useCursor(hovered);
+
+  return (
+    <>
+      <Suspense fallback={<Html></Html>}>
+        <Box args={[100, 100, 100]}>
+          <meshBasicMaterial color={quaternaryColor} side={THREE.DoubleSide} />
+          <group
+            position={[2, 2, 0]}
+            // rotation={[
+            //     (Math.PI * -2) / 16,
+            //     (Math.PI * -2) / 16,
+            //     (Math.PI * -1) / 16,
+            // ]}
+            onPointerOver={(e) => setHoverStatus(true)}
+            onPointerOut={(e) => setHoverStatus(false)}
+          >
+            {/* <Text
+                    fontSize={1}
+                    color={hovered ? secondaryColor : primaryColor}
+                    anchorX="center"
+                    anchorY="middle"
+                >
+                    Leon Lai
+                </Text> */}
+            <Word originalColor={primaryColor} transitionColor={secondaryColor}>
+              Leon Lai
+            </Word>
+            <group position={[0.05, 0.05, 0.00001]}>
+              <Text
+                color={quaternaryColor}
+                outlineWidth={0.02}
+                outlineColor={hovered ? secondaryColor : primaryColor}
+              >
+                Leon Lai
+              </Text>
+            </group>
+          </group>
+        </Box>
+        <PerspectiveCamera near={0.1} far={1000} />
+
+        <OrbitControls enableZoom={true} />
+      </Suspense>
+    </>
+  );
+};
+
+export default Contact;
