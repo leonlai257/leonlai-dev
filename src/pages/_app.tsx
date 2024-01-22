4b3747029d287e2ba5a7edad8d0c6340f9c58690
leonlai257 <leonlai257@gmail.com>
2024-01-21 22:09:39 -0500

    feat(icon): updated favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,7 +38,24 @@ export default function App({ pageProps }: AppProps) {
             <Head>
                 <title>Leon Lai</title>
                 <meta name="Leon Lai" content="Leon Lai" />
-                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
@@ -57,8 +74,6 @@ export default function App({ pageProps }: AppProps) {
             >
                 <ambientLight intensity={0.1} />
                 <Room colorProfile={pageProps.color} />
-
-                {/* <OrbitControls /> */}
 
                 <Switch>
                     <Route path="/">
