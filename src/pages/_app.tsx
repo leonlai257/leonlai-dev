169ea72b7908d07d092bf30445911daee8e8fd0a
leonlai257 <leonlai257@gmail.com>
2025-04-16 02:00:13 +0800

    chore: package.json & removed comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,14 +44,13 @@ export default function App({ Component, pageProps }: AppProps) {
                 <link rel="manifest" href="/site.webmanifest" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-   
+
             </Head>
 
             <NavBar />
             <div className="relative inset-x-0 m-auto flex w-full flex-col items-center justify-center overflow-x-hidden overflow-y-scroll bg-background pt-10 md:pt-[64px] pb-[100px]">
                 <div className="relative min-h-screen w-full max-w-[1440px] text-primary px-16">
                     <Component {...config} />
-                    {/* <Footer /> */}
                 </div>
             </div>
         </div>
